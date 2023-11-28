920fa171ace08c397dcf3ec9226e46d8534b1ea9
PetarVukovic <petarvukovic50@gmail.com>
2023-11-28 15:19:03 +0100

    Section 2

---
+++ API/Program.cs
@@ -0,0 +1,53 @@
+/*builder docs
+-Its going to create Kestrel server and host the application
+-It ll read our config file we pass to it.appsetting and dev.json.
+*/
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+/* Sercices docs
+Add services to the container.Services are our app logic and expend our functionalities.
+We ll use DI to inject our services to other classes.
+*/
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+/* Configure the HTTP request pipeline.
+Middleware.It can do something with http request in its way in and out 
+*/
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();//Registrira nase endpointove (kontrolere)
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (System.Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
+
+
+app.Run();
